69b6614fc728f860c13df1c8fbd79ae5b16e7e0b
BackwoodsBoomtree <rdoughty@caltech.edu>
2020-10-01 15:15:33 -0700

    Add CanopyLayers script to derive spectrum

---
+++ Julia_Scripts/derive_spectrum.jl
@@ -0,0 +1,52 @@
+using CSV
+using DataFrames
+using CanopyLayers
+
+struct SIFComparison2020{FT} end
+
+"""
+    derive_spectrum()
+Derive and plot the albedo and SIF spectrum
+"""
+function derive_spectrum()
+    FT = Float32;
+
+    # read data from file
+    # TODO add clumping factor
+    # TODO add cab
+    # replace the file name with your own file
+    #_file = joinpath(@__DIR__, "../../data/russ_data.csv");
+    _file = "C:/Russell/Projects/Geometry/R_Scripts/CSV/sif_ATTO_Tower_Manaus_Brazil_(incorrect)_2020-06-26.csv"
+    _data = DataFrame!(CSV.File(_file));
+
+    # initialize canopy radiation module
+    angles, can, can_opt, can_rad, in_rad,
+    leaves, rt_con, rt_dim, soil, wls = initialize_rt_module(FT);
+
+    # create a matrix to store the spectrum
+    mat_REF = zeros(FT, (length(_data.vza), length(wls.WL )));
+    mat_SIF = zeros(FT, (length(_data.vza), length(wls.WLF)));
+
+
+    # iterate through the data
+    for i in eachindex(_data.vza)
+        angles.tts = _data.sza[i];
+        angles.psi = _data.raa[i];
+        angles.tto = _data.vza[i];
+        can.LAI    = _data.lai_era5[i];
+        can.iLAI   = _data.lai_era5[i] * can.Ω / can.nLayer;
+
+        canopy_geometry!(can, angles, can_opt, rt_con);
+        canopy_matrices!(leaves, can_opt);
+        short_wave!(can, can_opt, can_rad, in_rad, soil, rt_con);
+        canopy_fluxes!(can, can_opt, can_rad, in_rad, soil, leaves, wls, rt_con);
+        SIF_fluxes!(leaves, can_opt, can_rad, can, soil, wls, rt_con, rt_dim);
+
+        mat_REF[i,:] .= can_rad.alb_obs;
+        mat_SIF[i,:] .= can_rad.SIF_obs;
+    end
+
+    return mat_REF, mat_SIF, wls.WLF
+end
+
+mat_REF, mat_SIF, wlf = derive_spectrum()
