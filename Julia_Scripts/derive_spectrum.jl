bc4bca0d9fc12f3a76397f09ef85962cba1c5975
Yujie Wang <jesiner@gmail.com>
2020-10-01 13:28:25 -0700

    simplify

--- Julia_Scripts/derive_spectrum.jl
+++ Julia_Scripts/derive_spectrum.jl
@@ -4,32 +4,22 @@
 # CSV
 # DataFrames
 # CanopyLayers
-# PlotPlants (optional, if you do not use my plotting commands)
 #
 
 struct SIFComparison2020{FT} end
 
 """
-    derive_spectrum(proj::SIFComparison2020{FT};
-                    saving::Bool,
-                    use_latex::Bool) where {FT<:AbstractFloat}
+    derive_spectrum()
 
-Derive and plot the albedo and SIF spectrum, given
-- `proj` [`SIFComparison2020`](@ref) type project control
-- `saving` Optional. If true, save the figure
-- `use_latex` Optional. If true, use latex and serif font
+Derive and plot the albedo and SIF spectrum
 """
-function derive_spectrum(
-            proj::SIFComparison2020{FT};
-            saving::Bool = false,
-            use_latex::Bool = true
-) where {FT<:AbstractFloat}
-    # use latex and serif font
-    if use_latex use_serif_tex(); end
+function derive_spectrum()
+    FT = Float32;
 
     # read data from file
     # TODO add clumping factor
     # TODO add cab
+    # replace the file name with your own file
     _file = joinpath(@__DIR__, "../../data/russ_data.csv");
     _data = DataFrame!(CSV.File(_file));
 
@@ -59,30 +49,5 @@ function derive_spectrum(
         mat_SIF[i,:] .= can_rad.SIF_obs;
     end
 
-    _fig1,_axes = create_canvas(1; figsize=(7.5,3.5));
-    _ax1 = _axes[1];
-    _cm1 = _ax1.pcolor(mat_REF);
-    _ax1.set_title("Reflectance Spetrum");
-    _ax1.set_xlabel("Wave Length Index");
-    _ax1.set_ylabel("Site Index");
-    _fig1.colorbar(_cm1, ax=_ax1, label="Albedo");
-    _fig1.set_tight_layout(true);
-
-    _fig2,_axes = create_canvas(2; figsize=(5.5,3.5));
-    _ax2 = _axes[1];
-    _cm2 = _ax2.pcolor(wls.WLF, eachindex(_data.vza), mat_SIF);
-    _ax2.set_title("SIF Spetrum");
-    _ax2.set_xlabel("Wave Length (nm)");
-    _ax2.set_ylabel("Site Index");
-    _fig2.colorbar(_cm2, ax=_ax2, label="SIF");
-    _fig2.set_tight_layout(true);
-
-    if saving
-        _fig1.savefig("figures/2020_sif_comparison/ref_spectrum.pdf",
-                      bbox_inches="tight");
-        _fig2.savefig("figures/2020_sif_comparison/sif_spectrum.pdf",
-                      bbox_inches="tight");
-    end
-
-    return nothing
+    return mat_REF, mat_SIF
 end
